2d023f3816bb6fd03a52fea65361ec7b02ebcb67
eduardo3g <eduardosbrasil10@gmail.com>
2020-09-30 20:24:04 -0300

    feat: add global styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
+import { Title } from '../styles/pages/Home';
+
 export default function Home() {
   return (
     <div>
-      <h1>Hello world</h1>
+      <Title>Hello world</Title>
     </div>
   )
 }
